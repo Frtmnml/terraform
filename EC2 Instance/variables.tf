813264b6b052416843df5c178522129e686c61b1
frtmnml <joshuacentenohrrr@gmaiail.com>
2024-02-25 19:51:19 -0600

    Adding tf files

---
+++ EC2 Instance/variables.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+    description     = "access_key"
+    type            = string
+    senstive        = true
+}
+
+variable "secret_key" {
+    description     = "access_key"
+    type            = string
+    senstive        = true
+}
